2887ff7ea89284847995b08661c233791d8f6435
phamtanphat <phatdroid94@gmail.com>
2023-07-12 18:15:25 +0700

    setup ui

--- app/src/main/java/com/example/recyclerview19052023/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview19052023/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.recyclerview19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerView: RecyclerView? = null
+    private var workAdapter: WorkAdapter? = null
+    private var listWorks: List<Work>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById(R.id.recyclerview)
     }
 }
