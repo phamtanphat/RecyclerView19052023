93705cefb8b3bf1cc34653843c829ad69f7a7289
phamtanphat <phatdroid94@gmail.com>
2023-07-12 19:24:33 +0700

    ui

--- app/src/main/java/com/example/recyclerview19052023/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview19052023/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.recyclerview19052023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private var recyclerView: RecyclerView? = null
     private var workAdapter: WorkAdapter? = null
-    private var listWorks: List<Work>? = null
+    private var listWorks: MutableList<Work>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,19 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerview)
 
-        listWorks = mutableListOf<Work>().apply {
-            add(Work("Title 1", "Do something 1"))
-            add(Work("Title 2", "Do something 2"))
-            add(Work("Title 3", "Do something 3"))
-            add(Work("Title 4", "Do something 4"))
-            add(Work("Title 5", "Do something 5"))
-        }
+        listWorks = mutableListOf()
         workAdapter = WorkAdapter(this, listWorks ?: mutableListOf())
         recyclerView?.adapter = workAdapter
 
         workAdapter?.setOnClickListener(object : WorkAdapter.OnClickListener {
             override fun onClick(position: Int) {
-                Toast.makeText(this@MainActivity, "Click delete", Toast.LENGTH_SHORT).show()
+                listWorks?.removeAt(position)
+                workAdapter?.notifyItemRemoved(position)
             }
         })
     }
