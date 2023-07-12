7ba385fee72911c56808b30e44387a596da6a406
phamtanphat <phatdroid94@gmail.com>
2023-07-12 18:03:51 +0700

    setup adapter continue

--- app/src/main/java/com/example/recyclerview19052023/WorkAdapter.kt
+++ app/src/main/java/com/example/recyclerview19052023/WorkAdapter.kt
@@ -3,6 +3,8 @@ package com.example.recyclerview19052023
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class WorkAdapter(
@@ -10,7 +12,14 @@ class WorkAdapter(
 ) : RecyclerView.Adapter<WorkAdapter.WorkViewHolder>(){
 
     inner class WorkViewHolder(var view: View): RecyclerView.ViewHolder(view) {
+        private var txtTile: TextView = view.findViewById(R.id.text_view_title)
+        private var txtDescription: TextView = view.findViewById(R.id.text_view_description)
+        private var imgDelete: ImageView = view.findViewById(R.id.image_view_delete)
 
+        fun bind(work: Work) {
+            txtTile.text = work.title
+            txtDescription.text = work.description
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkViewHolder {
@@ -22,6 +31,6 @@ class WorkAdapter(
     override fun getItemCount() = listWorks.size
 
     override fun onBindViewHolder(holder: WorkViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(listWorks[position])
     }
 }
